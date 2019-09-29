f623c5843d2be465b109f15dec9a5a817c13c21f
Dar <ksen.vl.set@gmail.com>
2019-09-29 12:39:55 +0300

    fix: pin.js

--- js/pin.js
+++ js/pin.js
@@ -3,15 +3,17 @@
 (function () {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+  var renderPinAttributs = function (pin) {
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.style.left = (pin.location.x - window.data.OFFSET_X) + 'px';
+    pinElement.style.top = (pin.location.y - window.data.OFFSET_Y) + 'px';
+    pinElement.querySelector('img').src = pin.author.avatar;
+    pinElement.querySelector('img').alt = pin.author.avatar;
+    return pinElement;
+  };
+
   window.pin = {
-    renderPinAttributs: function (pin) {
-      var pinElement = pinTemplate.cloneNode(true);
-      pinElement.style.left = (pin.location.x - window.data.OFFSET_X) + 'px';
-      pinElement.style.top = (pin.location.y - window.data.OFFSET_Y) + 'px';
-      pinElement.querySelector('img').src = pin.author.avatar;
-      pinElement.querySelector('img').alt = pin.author.avatar;
-      return pinElement;
-    }
+    renderPinAttributs: renderPinAttributs,
   };
 
 })();
